9a4b9e97003beec395a1a8df370ac864b11db5ab
deepak-raja-050 <dr26dr2006@gmail.com>
2026-09-23 21:53:46 +0530

    adding auth foundation

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,32 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
 const { MongoClient } = require("mongodb");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const app = express();
 
 const PORT = 5000;
 const mongoURL = "mongodb://127.0.0.1:27017";
-
 const client = new MongoClient(mongoURL);
 
 app.use(cors());
 app.use(express.json());
+
+function generateToken(user) {
+    return jwt.sign(
+        {
+            id: user.student_id,
+            role: user.role
+        },
+        process.env.JWT_SECRET,
+        {
+            expiresIn: "1h"
+        }
+    );
+}
 
 app.get("/", (req, res) => {
     res.send("InnoBridge Backend is running!");
@@ -19,7 +35,9 @@ app.get("/", (req, res) => {
 async function startServer() {
     try {
         await client.connect();
+
         const db = client.db("innobridge");
+
         console.log("MongoDB connected successfully");
         console.log("Database: innobridge");
 
